26b417126142be0c7e3782ee5c9b5daf161a6060
abdullateef <atandaabdullateef90@gmail.com>
2026-05-02 16:52:11 +0100

    new changes

---
+++ src/main/java/com/jpmc/midascore/entity/TransactionRecord.java
@@ -0,0 +1,52 @@
+package com.jpmc.midascore.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "transaction_records")
+public class TransactionRecord {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // Many transactions can have the same sender
+    @ManyToOne
+    @JoinColumn(name = "sender_id", nullable = false)
+    private UserRecord sender;
+
+    // Many transactions can have the same recipient
+    @ManyToOne
+    @JoinColumn(name = "recipient_id", nullable = false)
+    private UserRecord recipient;
+
+    @Column(nullable = false)
+    private float amount;
+
+    public TransactionRecord() {}
+
+    public TransactionRecord(UserRecord sender, 
+                             UserRecord recipient, 
+                             float amount) {
+        this.sender = sender;
+        this.recipient = recipient;
+        this.amount = amount;
+    }
+
+    public Long getId() { return id; }
+    public UserRecord getSender() { return sender; }
+    public void setSender(UserRecord sender) { this.sender = sender; }
+    public UserRecord getRecipient() { return recipient; }
+    public void setRecipient(UserRecord recipient) { 
+        this.recipient = recipient; 
+    }
+    public float getAmount() { return amount; }
+    public void setAmount(float amount) { this.amount = amount; }
+}
